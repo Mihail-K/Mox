71732cf849925c8dd513807fa0825a0f815970f9
Mihail-K <Mihail.K@fimu.tk>
2015-11-29 15:41:49 -0500

    Add test code to Main.

--- src/mox/Main.java
+++ src/mox/Main.java
@@ -6,18 +6,39 @@
 
 package mox;
 
+import mox.states.TestState;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 /**
  *
  * @author Mihail
  */
-public class Main
+public class Main extends StateBasedGame
 {
+
+    public Main(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException
+    {
+        this.addState(new TestState());
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws SlickException
     {
+        AppGameContainer app = new AppGameContainer(new Main("Test"));
+        app.setDisplayMode(800, 600, false);
+        app.setShowFPS(true);
+        app.start();
     }
 
 }
