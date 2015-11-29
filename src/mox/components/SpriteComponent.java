c45331ed031124cb61d42ce6914597aece2ede19
Mihail-K <Mihail.K@fimu.tk>
2015-11-29 14:37:56 -0500

    Add sprite component.

---
+++ src/mox/components/SpriteComponent.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mox.components;
+
+import org.newdawn.slick.geom.Shape;
+
+/**
+ *
+ * @author Mihail
+ */
+public class SpriteComponent implements Component
+{
+    
+    private final Shape shape;
+    private boolean visible;
+    
+    public SpriteComponent(Shape shape)
+    {
+        this(shape, true);
+    }
+    
+    public SpriteComponent(Shape shape, boolean visible)
+    {
+        this.shape = shape;
+        this.visible = visible;
+    }
+    
+    public Shape getShape()
+    {
+        return shape;
+    }
+    
+    public boolean isVisible()
+    {
+        return visible;
+    }
+    
+    public void setVisible(boolean visible)
+    {
+        this.visible = visible;
+    }
+    
+}
