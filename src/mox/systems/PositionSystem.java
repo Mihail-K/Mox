869509a9e7e37311e525f1fa34ddc445b606d026
Mihail-K <Mihail.K@fimu.tk>
2015-11-28 21:21:32 -0500

    Add position system.

---
+++ src/mox/systems/PositionSystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mox.systems;
+
+import mox.entities.EntityManager;
+
+/**
+ *
+ * @author Mihail
+ */
+public class PositionSystem extends System
+{
+
+    public PositionSystem(EntityManager manager)
+    {
+        super(manager);
+    }
+
+    @Override
+    public void update(double delta)
+    {
+    }
+    
+}
