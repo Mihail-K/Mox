4a415efe19007d7fafe16d461393f5c08a07828f
Mihail-K <Mihail.K@fimu.tk>
2015-11-29 15:24:11 -0500

    Add PositionChangedMessage.

---
+++ src/mox/messages/PositionChangedMessage.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mox.messages;
+
+import org.newdawn.slick.geom.Vector2f;
+
+/**
+ *
+ * @author Mihail
+ */
+public class PositionChangedMessage extends Message
+{
+    
+    private final long handle;
+    private final Vector2f position;
+
+    public PositionChangedMessage(long handle, Vector2f position)
+    {
+        super("position-changed");
+        
+        this.handle = handle;
+        this.position = position;
+    }
+
+    public long getHandle()
+    {
+        return handle;
+    }
+
+    public Vector2f getPosition()
+    {
+        return position;
+    }
+    
+}
